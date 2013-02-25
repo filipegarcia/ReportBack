6a8109a776da2cc917920488d2c49cb9a11dcc95
filipe garcia <filipe.c.garcia@gmail.com>
2013-02-25 15:11:13 +0100

    messing around with uglify
    Signed-off-by: filipe garcia <filipe.c.garcia@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function (grunt) {
-  'use strict';
+
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-css');
 
 grunt.initConfig({
 
@@ -10,12 +12,40 @@ grunt.initConfig({
     main: {
       files: {
         'assets/js/report-back.min.js': [
-        'assets/js/report-back.js',
-        'assets/js/html2canvas.js'
+        'assets/js/report-back.js'
+      ]
+      }
+    },
+    libs: {
+      files: {
+        'assets/js/dependencies.js': [
+        'assets/js/jquery.base64.js',
+        'assets/js/html2canvas.js',
+        'bootstrap.min.js'
       ]
       }
     }
+  },
+
+  cssmin: {
+    options: {
+      keepSpecialComments: 0
+    },
+    libs: {
+      src: ['assets/css/bootstrap.min.css',  'assets/css/jquery.ui.css'],
+      dest: 'assets/css/deps.css'
+    }
+  },
+  jshint: {
+    options: {
+      asi: true,
+      smarttabs: true
+    },
+    all: ['Gruntfile.js', 'assets/js/report-back.js']
   }
+
+
+
 });
 
 
@@ -23,6 +53,8 @@ grunt.initConfig({
       'default',
       [
           'uglify',
+          //'cssmin',
+          //'jshint'
       ]
   );
-}
+};
